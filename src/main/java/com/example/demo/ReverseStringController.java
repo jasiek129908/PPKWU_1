0aa64b453692af14efc21e83ef9d0b4ba8778432
Jan Oleksik <222529@edu.p.lodz.pl>
2021-10-14 12:50:35 +0200

    added fake implementation

--- src/main/java/com/example/demo/ReverseStringController.java
+++ src/main/java/com/example/demo/ReverseStringController.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReverseStringController {
 
     @GetMapping(value = "/rev")
-    getReverseString() {
-
+    String getReverseString(@RequestParam String text) {
+        return text;
     }
 
 }
